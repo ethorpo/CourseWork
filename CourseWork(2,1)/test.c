d277119f99daacbc158360b7f4f493c96cfc80a2
ethorpo <32637416+ethorpo@users.noreply.github.com>
2018-02-18 17:02:35 +0000

    Testing continues

--- CourseWork(2,1)/test.c
+++ CourseWork(2,1)/test.c
@@ -1,105 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <assert.h>
 
-char** str_split(char* a_str, const char a_delim)
-{
-	char** result = 0;
-	size_t count = 0;
-	char* tmp = a_str;
-	char* last_comma = 0;
-	char delim[2];
-	delim[0] = a_delim;
-	delim[1] = 0;
-
-	/* Count how many elements will be extracted. */
-	while (*tmp)
-	{
-		if (a_delim == *tmp)
-		{
-			count++;
-			last_comma = tmp;
-		}
-		tmp++;
-	}
-
-	/* Add space for trailing token. */
-	count += last_comma < (a_str + strlen(a_str) - 1);
-
-	/* Add space for terminating null string so caller
-	knows where the list of returned strings ends. */
-	count++;
-
-	result = malloc(sizeof(char*) * count);
-
-	if (result)
-	{
-		size_t idx = 0;
-		char* token = strtok(a_str, delim);
-
-		while (token)
-		{
-			assert(idx < count);
-			*(result + idx++) = strdup(token);
-			token = strtok(0, delim);
-		}
-		assert(idx == count - 1);
-		*(result + idx) = 0;
-	}
-
-	return result;
+void fatal(char *msg) {
+    printf("%s\n", msg);
+    exit (1);
 }
 
-int main()
+int main(int argc, char*argv[])
 {
-	int lines = 0;
-	char ch;
-	int wordcount = 0;
-	int charcount = 0;
+    int col1, col2;
+    FILE *fp;
+    char ch[256];
 
-	FILE *fileIn;
-	fileIn = fopen("input.txt", "r");
+    if(argc < 4)
+        fatal("Need three arguments");
 
-	FILE *fileOut;
-	fileOut = fopen("output.txt", "w");
+    if (sscanf(argv[2], "%d", &col1) != 1)
+        fatal("Argument 1 error");
 
-	char wordArray[100];
-	fgets(wordArray, 100, (FILE*)fileIn);
-	char** tokens;
+    if (sscanf(argv[3], "%d", &col2) != 1)
+        fatal("Argument 2 error");
 
-	tokens = str_split(wordArray, ' ');
+    printf("Read columns %d and %d from file %s\n", col1, col2, argv[1]);
+    if ((fp = fopen(argv[1], "r")) == NULL)
+        fatal("Unable to open file");
 
-	if (tokens)
-	{
-		int i;
-		for (i = 0; *(tokens + i); i++)
-		{
-			fprintf(fileOut, "%s\n", *(tokens + i));
-			free(*(tokens + i));
-			lines++;
-		}
-		free(tokens);
-	}
-		//Repeat until End Of File character is reached.	
-		while ((ch = getc(fileIn)) != EOF) {
-
-			if (ch != ' ' && ch != '\n') 
-			{
-				++charcount;
-				
-			}
-			// Increment word count if new line or space character
-			if (ch == ' ' || ch == '\n') { ++wordcount; }
-
-		}
-
-		if (charcount > 0) {
-
-			++wordcount;
-		}
-
-		fprintf(fileOut, "%d\n", wordcount);
-
-	return 0;
+    // process the file
+    while (fgets(ch, 256, fp) != NULL) {
+        printf("%s", ch);
+    }
+    fclose(fp);
+    return 0;
 }
--- CourseWork(2,1)/test2.c
+++ CourseWork(2,1)/test2.c
@@ -1,54 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char*argv[])
 {
-	FILE *fp;
-	char filename[100];
-	char ch;
-	int linecount, wordcount, charcount;
 
-	// Initialize counter variables
-	linecount = 0;
-	wordcount = 0;
-	charcount = 0;
+    FILE *fpIn;
+	FILE *fpOut;
+    char ch[256];
+	char comLine[2];
+	char arg[256];
+	const char *in[2];
+	in[0] = "-";
+	in[1] = "i";
+	const char *out[2];
+	out[0] = "-";
+	out[1] = "o";
+	int inYes;
+	int outYes;
+	int inTrue = 0;
+	int outTrue = 0;
 
-	// Prompt user to enter filename
-	printf("Enter a filename :");
-	gets(filename);
-
-	// Open file in read-only mode
-	fp = fopen(filename, "r");
-
-	// If file opened successfully, then write the string to file
-	if (fp)
+	for (int i = 1; i < 5; i++)
 	{
-		//Repeat until End Of File character is reached.	
-		while ((ch = getc(fp)) != EOF) {
-			// Increment character count if NOT new line or space
-			if (ch != ' ' && ch != '\n') { ++charcount; }
 
-			// Increment word count if new line or space character
-			if (ch == ' ' || ch == '\n') { ++wordcount; }
+		arg[i] = sscanf(argv[i]);
 
-			// Increment line count if new line character
-			if (ch == '\n') { ++linecount; }
 
+		inYes = int strcmp((sscanf(argv[i])), (comLine));
+		outYes = int strcmp((sscanf(argv[i])), (comLine));
+
+		if (inYes == 0)
+		{
+			fpIn = fopen(argv[i + 1], "r");
+			inTrue = 1;
 		}
 
-		if (charcount > 0) {
-			++linecount;
-			++wordcount;
+		if (outYes == 0)
+		{
+			fpOut = fopen(argv[i + 1], "w");
+			outTrue = 1;
 		}
 	}
-	else
+
+	if (inTrue == 0)
 	{
-		printf("Failed to open the file\n");
+		printf ("Enter text: ");
+		gets(ch);
 	}
 
-	printf("Lines : %d \n", linecount);
-	printf("Words : %d \n", wordcount);
-	printf("Characters : %d \n", charcount);
+	if (outTrue == 0)
+	{
+	printf ("%s", ch);
+	}
 
-	getchar();
-	return(0);
+    return 0;
 }
