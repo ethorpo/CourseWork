c060a91392e6570a278ee88d25da2e9010962d11
ethorpo <32637416+ethorpo@users.noreply.github.com>
2018-02-22 12:58:05 +0000

    Getting there

--- CourseWork(2,1)/test2.c
+++ CourseWork(2,1)/test2.c
@@ -1,56 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, char*argv[])
 {
 
-    FILE *fpIn;
-	FILE *fpOut;
+	FILE *fileIn;
+	FILE *fileOut;
+	char* inputFile = "in";
+	char* outputFile = "out";
     char ch[256];
-	char comLine[2];
-	char arg[256];
-	const char *in[2];
-	in[0] = "-";
-	in[1] = "i";
-	const char *out[2];
-	out[0] = "-";
-	out[1] = "o";
-	int inYes;
-	int outYes;
-	int inTrue = 0;
-	int outTrue = 0;
 
-	for (int i = 1; i < 5; i++)
+	for (int i = 1; i < argc; i++)
 	{
 
-		arg[i] = sscanf(argv[i]);
-
-
-		inYes = int strcmp((sscanf(argv[i])), (comLine));
-		outYes = int strcmp((sscanf(argv[i])), (comLine));
-
-		if (inYes == 0)
+		if (strcmp(argv[i], "-i") == 0)
 		{
-			fpIn = fopen(argv[i + 1], "r");
-			inTrue = 1;
+			inputFile = argv[i + 1];
+			i++;
 		}
 
-		if (outYes == 0)
+		if (strcmp(argv[i], "-o") == 0)
 		{
-			fpOut = fopen(argv[i + 1], "w");
-			outTrue = 1;
+			outputFile = argv[i + 1];
+			i++;
 		}
 	}
 
-	if (inTrue == 0)
+	if (strcmp(inputFile, "in") != 0)
 	{
-		printf ("Enter text: ");
+		fileIn = fopen(inputFile, "r");
+		//
+	}
+	else
+	{
+		printf("Enter text: ");
 		gets(ch);
 	}
 
-	if (outTrue == 0)
+	if (strcmp(outputFile, "out") != 0)
+	{	
+		fileOut = fopen(outputFile, "w");
+		fprintf(fileOut, "%s", ch);
+	}	
+	else
 	{
-	printf ("%s", ch);
+		printf("%s", ch);
 	}
 
     return 0;
--- CourseWork(2,1)/test3.c
+++ CourseWork(2,1)/test3.c
@@ -10,7 +10,7 @@ int cmpfunc (const void * a, const void * b)
 
 enum { MAX_WORDS = 1000, MAX_WORD_LEN = 32 };
 
-int main(void)
+int main(int argc, char*argv[])
 {
     char words[MAX_WORDS][MAX_WORD_LEN];
     int  count[MAX_WORDS] = { 0 };
@@ -19,61 +19,122 @@ int main(void)
     int c;
     int l = 0;
 	int totalWords = 0;
-
 	FILE *fileIn;
-	fileIn = fopen("input.txt", "r");
-
 	FILE *fileOut;
-	fileOut = fopen("output.txt", "w");
+	char* inputFile = "in";
+	char* outputFile = "out";
+	char ch[256];
 
-    while ((c = getc(fileIn)) != EOF)
-    {
-        if (isalpha(c))
-        {
-           if (l < MAX_WORD_LEN - 1)
-               word[l++] = c;
-            else
-            {
-               // fprintf(stderr, "Word too long: %*s%c...\n", l, word, c);
-                break;
-            } 
-        }
-        else if (l > 0)
-        {
-            word[l] = '\0';
-  //          printf( "Found word <<%s>>\n", word);
-            assert(strlen(word) < MAX_WORD_LEN);
-            int found = 0;
-            for (int i = 0; i < w; i++)
-            {
-                if (strcmp(word, words[i]) == 0)
-                {
-                    count[i]++;
-                    found = 1;
-                    break;
-                }
-            }
-            if (!found)
-            {
-                if (w >= MAX_WORDS)
-                {
-                  //  fprintf(stderr, "Too many distinct words (%s)\n", word);
-                    break;
-                }
-                strcpy(words[w], word);
-                count[w++] = 1;
-            }
-            l = 0;
-        }
-    }
+	for (int i = 1; i < argc; i++)
+	{
 
-	qsort(count, w, sizeof(int *), cmpfunc);
-
-    for (int i = 0; i < w; i++)
+		if (strcmp(argv[i], "-i") == 0)
 		{
-		totalWords++;
-        fprintf(fileOut, "%s:%3d\n", words[i], count[i]);
+			inputFile = argv[i + 1];
+			i++;
+		}
+
+		if (strcmp(argv[i], "-o") == 0)
+		{
+			outputFile = argv[i + 1];
+			i++;
+		}
+	}
+
+	if (strcmp(inputFile, "in") != 0)
+	{
+		fileIn = fopen(inputFile, "r");
+
+		while ((c = getc(fileIn)) != EOF)
+		{
+			if (isalpha(c))
+			{
+				word[l++] = c;
+			}
+			else if (l > 0)
+			{
+				word[l] = '\0';
+				//printf( "Found word <<%s>>\n", word);
+				int found = 0;
+				for (int i = 0; i < w; i++)
+				{
+					if (strcmp(word, words[i]) == 0)
+					{
+						count[i]++;
+						found = 1;
+						break;
+					}
+				}
+				if (!found)
+				{
+					strcpy(words[w], word);
+					count[w++] = 1;
+				}
+				l = 0;
+			}
+		}
+
+	}
+	else
+	{
+		printf("Enter text: ");
+		gets(ch);
+
+		for (int i = 0; i < sizeof(ch); i++)
+		{
+			c = ch[i];
+
+			if (isalpha(c))
+			{
+				word[l++] = c;
+			}
+			else if (l > 0)
+			{
+				word[l] = '\0';
+				//printf( "Found word <<%s>>\n", word);
+				int found = 0;
+				for (int i = 0; i < w; i++)
+				{
+					if (strcmp(word, words[i]) == 0)
+					{
+						count[i]++;
+						found = 1;
+						break;
+					}
+				}
+				if (!found)
+				{
+					strcpy(words[w], word);
+					count[w++] = 1;
+				}
+				l = 0;
+			}
+		}
+
+	}
+
+	if (strcmp(outputFile, "out") != 0)
+	{
+		fileOut = fopen(outputFile, "w");
+		for (int i = 0; i < w; i++)
+		{
+			totalWords++;
+			fprintf(fileOut, "%s:%d\n", words[i], count[i]);
 		}
 		fprintf(fileOut, "Total Words: %d", totalWords);
+	}
+	else
+	{
+		for (int i = 0; i < w; i++)
+		{
+			totalWords++;
+			printf("%s:%d\n", words[i], count[i]);
+		}
+		printf("Total Words: %d", totalWords);
+	}
+
+	//qsort(count, w, sizeof(int *), cmpfunc);
+
+    
     return 0;
 }
